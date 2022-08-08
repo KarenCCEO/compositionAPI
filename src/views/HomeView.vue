6bedf3b9c65fa5b7d6e6045fb30dbe91e04d3f40
KarenCCEO <karen.suarez@cceo.com.mx>
2022-08-08 15:16:48 -0500

    composition api para botones aumentar y disminuir

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Contador:{{contador}}</h1>
+    <button @click="aumentar">+</button>
+    <button @click="disminuir">-</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      contador: 0
+    }
+
+  },
+  methods:{
+    aumentar(){
+      this.contador ++
+    },
+    disminuir(){
+      this.contador --
+    }
   }
+  
 }
 </script>
